246b583956e0a4990e806848c36353b5d602d066
AceMood <zmike86@163.com>
2016-01-13 18:31:20 +0800

    add graph test case

--- test/graph-Spec.js
+++ test/graph-Spec.js
@@ -20,15 +20,15 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- * @file 循环依赖测试
+ * @file 有向图遍历测试
  * @author AceMood
  * @email zmike86@gmail.com
  */
 
-describe('Circular', function() {
+describe('Traverse graph', function() {
 
   var expect = require('chai').expect;
-  var circle = require('../lib/circle');
+  var Graph = require('../lib/graph');
 
   it('should pass simple Topological dependency', function() {
     var tree = {
@@ -140,3 +140,33 @@ describe('Circular', function() {
   });
 
 });
+
+
+var G = require('./graph');
+var V = require('./vertex');
+
+
+var v1 = new V('base/util.js', []);
+var v2 = new V('base/string.js', [v1]);
+var v3 = new V('base/uri.js', []);
+var v4 = new V('base/ajax.js', [v1, v2, v3]);
+
+var v14 = new V('widget/footer.js', [v1]);
+var v15 = new V('widget/topbar.js', [v1, v4]);
+var v16 = new V('widget/feed.js', [v2, v4, v2, v3]);
+
+var v5 = new V('common/api.js', [v2]);
+var v6 = new V('common/msg.js', []);
+var v7 = new V('common/dialog.js', [v2, v4, v5, v6]);
+var v8 = new V('common/slide.js', [v2, v4, v5, v6]);
+
+var v9 = new V('app/warn.js', [v1, v7, v8, v4]);
+var v10 = new V('app/addGroup.js', [v2, v7]);
+var v11 = new V('app/table.js', [v2, v4, v10]);
+var v12 = new V('app/slide.js', [v1, v2]);
+var v13 = new V('app/index.js', [v1, v9, v10, v11, v12, v14, v15, v16]);
+
+var g = new G();
+g.setRoot(v13);
+g.bfs();
+g.print();
